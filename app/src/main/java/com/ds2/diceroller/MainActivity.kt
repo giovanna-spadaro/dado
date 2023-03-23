dcff13100ffce71e329a27c31966404fd224dba8
giovanna-spadaro <gipekenas@hotmail.com>
2023-03-22 21:20:23 -0300

    atualização

--- app/src/main/java/com/ds2/diceroller/MainActivity.kt
+++ app/src/main/java/com/ds2/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ds2.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -25,20 +26,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun rollDice() {
-        val dice = dice(6)
+        val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-}
 
-}
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
-
-
-class dice(private val numSides: Int){
-
-            fun roll(): Int{
-                return (1..numSides). random()
-
-            }
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+}
+
+
+
+
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+
+    }
+}
+}
